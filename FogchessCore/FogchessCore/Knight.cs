b9f012afa2cbfe942ae0497103d5b73fa1900c42
Rohit Patnaik <quanticle@quanticle.net>
2014-12-30 11:23:59 -0600

    Added class and test class for knights

---
+++ FogchessCore/FogchessCore/Knight.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FogchessCore
+{
+    public class Knight : ChessPiece
+    {
+        public Knight(PieceColor color)
+        {
+            this._color = color;
+            this._type = PieceType.Knight;
+        }
+        public override void RevealVisibleSquares()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FogchessCoreTests/KnightTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FogchessCoreTests
+{
+    [TestClass]
+    public class KnightTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
