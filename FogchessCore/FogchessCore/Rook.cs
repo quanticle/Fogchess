f8db539d9b3e29cbc5668e8f3fc085b9575f25cb
Rohit Patnaik <quanticle@quanticle.net>
2014-12-30 23:10:50 -0600

    Wrote vision computation for rook and started unit tests for rook visibility computation.

--- FogchessCore/FogchessCore/Rook.cs
+++ FogchessCore/FogchessCore/Rook.cs
@@ -6,7 +6,55 @@ using System.Threading.Tasks;
 
 namespace FogchessCore
 {
-    class Rook
+    public class Rook : ChessPiece
     {
+        public Rook(PieceColor color)
+        {
+            this._type = PieceType.Rook;
+            this._color = color;
+        }
+
+        /// <summary>
+        /// Rooks look straight ahead and to the left and right
+        /// </summary>
+        public override void RevealVisibleSquares()
+        {
+            int currentRank = this.Square.Rank;
+            int currentFile = this.Square.File;
+            ChessBoard board = this.Square.Board;
+            this.Square.Board.Grid[currentRank][currentFile].Visible = true;
+
+            //Reveal squares forward
+            int revealedRank = currentRank + 1;
+            while (revealedRank < 8 && board.Grid[revealedRank][currentFile].Piece == null)
+            {
+                board.Grid[revealedRank][currentFile].Visible = true;
+                revealedRank++;
+            }
+
+            //Reveal squares backwards
+            revealedRank = currentRank - 1;
+            while (revealedRank >= 0 && board.Grid[revealedRank][currentFile].Piece == null)
+            {
+                board.Grid[revealedRank][currentFile].Visible = true;
+                revealedRank--;
+            }
+
+            //Reveal squares left
+            int revealedFile = currentFile - 1;
+            while (revealedFile >= 0 && board.Grid[currentRank][revealedFile].Piece == null)
+            {
+                board.Grid[currentRank][revealedFile].Visible = true;
+                revealedFile--;
+            }
+
+            //Reveal squares right
+            revealedFile = currentFile + 1;
+            while (revealedFile < 8 && board.Grid[currentRank][revealedFile].Piece == null)
+            {
+                board.Grid[currentRank][revealedFile].Visible = true;
+                revealedFile++;
+            }
+        }
     }
 }
--- FogchessCoreTests/RookTests.cs
+++ FogchessCoreTests/RookTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FogchessCore;
 
 namespace FogchessCoreTests
 {
@@ -7,8 +8,83 @@ namespace FogchessCoreTests
     public class RookTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestRookVisibilityCalculation_LowerLeftCorner_EmptyBoard()
         {
+            ChessBoard board = new ChessBoard();
+            board.Grid[0][0].Piece = new Rook(PieceColor.White)
+            {
+                Square = board.Grid[0][0]
+            };
+            board.Grid[0][0].Piece.RevealVisibleSquares();
+            for (int i = 0; i < 8; i++)
+            {
+                Assert.AreEqual(true, board.Grid[i][0].Visible);
+                Assert.AreEqual(true, board.Grid[0][i].Visible);
+            }
+        }
+
+        [TestMethod]
+        public void TestRookVisibilityCalculation_LowerRightCorner_EmptyBoard()
+        {
+            ChessBoard board = new ChessBoard();
+            board.Grid[0][7].Piece = new Rook(PieceColor.White)
+            {
+                Square = board.Grid[0][7]
+            };
+            board.Grid[0][7].Piece.RevealVisibleSquares();
+            for (int i = 0; i < 7; i++)
+            {
+                Assert.AreEqual(true, board.Grid[i][7].Visible);
+                Assert.AreEqual(true, board.Grid[0][i].Visible);
+            }
+        }
+
+        [TestMethod]
+        public void TestRookVisibilityCalculation_TopLeftCorner_EmptyBoard()
+        {
+            ChessBoard board = new ChessBoard();
+            board.Grid[7][0].Piece = new Rook(PieceColor.White)
+            {
+                Square = board.Grid[7][0]
+            };
+            board.Grid[7][0].Piece.RevealVisibleSquares();
+            for (int i = 7; i >= 0; i--)
+            {
+                Assert.AreEqual(true, board.Grid[i][0].Visible);
+                Assert.AreEqual(true, board.Grid[7][i].Visible);
+            }
+        }
+
+        [TestMethod]
+        public void TestRookVisibilityCalculation_TopRightCorner_EmptyBoard()
+        {
+            ChessBoard board = new ChessBoard();
+            board.Grid[7][7].Piece = new Rook(PieceColor.White)
+            {
+                Square = board.Grid[7][7]
+            };
+            board.Grid[7][7].Piece.RevealVisibleSquares();
+            for (int i = 7; i >= 0; i--)
+            {
+                Assert.AreEqual(true, board.Grid[i][7].Visible);
+                Assert.AreEqual(true, board.Grid[7][i].Visible);
+            }
+        }
+
+        [TestMethod]
+        public void TestRookVisibilityCalculation_Middle_EmptyBoard()
+        {
+            ChessBoard board = new ChessBoard();
+            board.Grid[4][4].Piece = new Rook(PieceColor.White)
+            {
+                Square = board.Grid[4][4]
+            };
+            board.Grid[4][4].Piece.RevealVisibleSquares();
+            for (int i = 0; i < 7; i++)
+            {
+                Assert.AreEqual(true, board.Grid[i][4].Visible);
+                Assert.AreEqual(true, board.Grid[4][i].Visible);
+            }
         }
     }
 }
